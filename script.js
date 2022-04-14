cb7cc2032905cd8839c48e05bea338b9bdb537dd
Noden Sera <nodensera@gmail.com>
2022-04-14 15:50:06 -0300

    nav menu & slider

--- script.js
+++ script.js
@@ -1,96 +1,56 @@
-var slideIndex, slides, dots, captionText;
-function initGallery() {
-  slideIndex = 0;
-  slides = document.getElementsByClassName('imageHolder');
-  slides[slideIndex].style.opacity = 1;
+const menuBtn = document.querySelector('.menu-div');
+const exitBtn = document.querySelector('.exit');
 
-  captionText = document.querySelector('.captionTextHolder .captionText');
-  captionText.innerText =
-    slides[slideIndex].querySelector('.captionText').innerText;
+let t1 = gsap.timeline({ paused: true });
+t1.to('.menu', { opacity: 1, duration: 1, top: 0, ease: Power2.easeInOut });
+t1.to(
+  '.nav',
+  {
+    opacity: 1,
+    marginBottom: 0,
+    duration: 1,
+    ease: Power2.easeInOut,
+    stagger: 0.3,
+  },
+  '>-0.5'
+);
 
-  //disable nextPrevBtn if slide count is one
-  if (slides.length < 2) {
-    var nextPrevBtns = document.querySelector('.leftArrow,.rightArrow');
-    nextPrevBtns.style.display = 'none';
-    for (i = 0; i < nextPrevBtn.length; i++) {
-      nextPrevBtn[i].style.display = 'none';
-    }
-  }
+menuBtn.addEventListener('click', () => {
+  t1.play().timeScale(1);
+});
 
-  //add dots
-  dots = [];
-  var dotsContainer = document.getElementById('dotsContainer'),
-    i;
-  for (i = 0; i < slides.length; i++) {
-    var dot = document.createElement('span');
-    dot.classList.add('dots');
-    dotsContainer.append(dot);
-    dot.setAttribute('onclick', 'moveSlide(' + i + ')');
-    dots.push(dot);
-  }
-  dots[slideIndex].classList.add('active');
-}
-initGallery();
-function plusSlides(n) {
-  moveSlide(slideIndex + n);
-}
-function moveSlide(n) {
-  var i;
-  var current, next;
-  var moveSlideAnimClass = {
-    forCurrent: '',
-    forNext: '',
-  };
-  var slideTextAnimClass;
-  if (n > slideIndex) {
-    if (n >= slides.length) {
-      n = 0;
-    }
-    moveSlideAnimClass.forCurrent = 'moveLeftCurrentSlide';
-    moveSlideAnimClass.forNext = 'moveLeftNextSlide';
-    slideTextAnimClass = 'slideTextFromTop';
-  } else if (n < slideIndex) {
-    if (n < 0) {
-      n = slides.length - 1;
-    }
-    moveSlideAnimClass.forCurrent = 'moveRightCurrentSlide';
-    moveSlideAnimClass.forNext = 'moveRightPrevSlide';
-    slideTextAnimClass = 'slideTextFromBottom';
-  }
+exitBtn.addEventListener('click', () => {
+  t1.timeScale(2.5);
+  t1.reverse();
+});
 
-  if (n != slideIndex) {
-    next = slides[n];
-    current = slides[slideIndex];
-    for (i = 0; i < slides.length; i++) {
-      slides[i].className = 'imageHolder';
-      slides[i].style.opacity = 0;
-      dots[i].classList.remove('active');
-    }
-    current.classList.add(moveSlideAnimClass.forCurrent);
-    next.classList.add(moveSlideAnimClass.forNext);
-    dots[n].classList.add('active');
-    slideIndex = n;
-    captionText.style.display = 'none';
-    captionText.className = 'captionText ' + slideTextAnimClass;
-    captionText.innerText = slides[n].querySelector('.captionText').innerText;
-    captionText.style.display = 'block';
-  }
-}
-var timer = null;
-function setTimer() {
-  timer = setInterval(function () {
-    plusSlides(1);
-  }, 3000);
-}
-setTimer();
-function playPauseSlides() {
-  var playPauseBtn = document.getElementById('playPause');
-  if (timer == null) {
-    setTimer();
-    playPauseBtn.style.backgroundPositionY = '0px';
-  } else {
-    clearInterval(timer);
-    timer = null;
-    playPauseBtn.style.backgroundPositionY = '-33px';
-  }
-}
+
+//slider
+
+var sickPrimary = {
+  autoplay: true,
+  autoplaySpeed: 2400,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  speed: 1800,
+  cssEase: 'cubic-bezier(.84, 0, .08, .99)',
+  asNavFor: '.text-slider',
+  centerMode: true,
+  prevArrow: $('.prev'),
+  nextArrow: $('.next'),
+};
+
+var sickSecondary = {
+  autoplay: true,
+  autoplaySpeed: 2400,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  speed: 1800,
+  cssEase: 'cubic-bezier(.84, 0, .08, .99)',
+  asNavFor: '.image-slider',
+  prevArrow: $('.prev'),
+  nextArrow: $('.next'),
+};
+
+$('.image-slider').slick(sickPrimary);
+$('.text-slider').slick(sickSecondary);
